04d636930b4ee28b457f2b026c7b0f5f6d217943
Hri <hri.gh@outlook.com>
2026-05-14 20:47:49 +0530

    feat: add MongoDB client and database connection functions

---
+++ src/lib/db.ts
@@ -0,0 +1,14 @@
+import clientPromise from "./mongodb";
+import type { Insight } from "@/types/insight.types";
+
+export async function getDatabase() {
+    const client = await clientPromise;
+    const db = client.db("dashboard");
+    console.log("#####################Database Connected#####################");
+    return db;
+}
+
+export async function getInsightsCollection() {
+    const db = await getDatabase();
+    return db.collection<Insight>("insights");
+}
